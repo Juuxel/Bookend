cacc9b7906f28416de2839d5a41a42965caa041f
Juuz <6596629+Juuxel@users.noreply.github.com>
2026-03-10 18:49:16 +0200

    Add license

--- src/main/java/juuxel/bookend/storage/BookDb.java
+++ src/main/java/juuxel/bookend/storage/BookDb.java
@@ -1,7 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package juuxel.bookend.storage;
 
 import juuxel.bookend.util.Logging;
-import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 
 import java.io.File;
